447a768aea5c9a0e05a787169981d138b39e8052
ZetaCleaner <dazn03@gmx.de>
2024-10-05 22:33:00 +0200

    Update PC-Check.ps1

--- PC-Check.ps1
+++ PC-Check.ps1
@@ -147,7 +147,7 @@ $o1 = & {
     "Last Boot up Time: $((Get-CimInstance Win32_OperatingSystem).LastBootUpTime | Get-Date -Format 'dd/MM/yyyy HH:mm:ss')" 
     
     "Last Recycle Bin Clear: $((Get-PSDrive -PSProvider FileSystem | ForEach-Object { Get-ChildItem -Path (Join-Path -Path $_.Root -ChildPath '$Recycle.Bin') -Force -ErrorAction SilentlyContinue } | Sort-Object LastWriteTime -Descending | Select-Object -Index 9).LastWriteTime.ToString('dd/MM/yyyy HH:mm:ss'))"
-    }
+    
     $sysUptime = "System-Uptime: $((New-TimeSpan -Start (Get-CimInstance Win32_OperatingSystem).LastBootUpTime -End (Get-Date)) | ForEach-Object { "$($_.Days) Days, {0:D2}:{1:D2}:{2:D2}" -f $_.Hours, $_.Minutes, $_.Seconds })"
 
 $documentspath = [System.Environment]::GetFolderPath('MyDocuments')
