0c4a78815c490c080e8844c36b2818642593e036
ZetaCleaner <dazn03@gmx.de>
2024-10-05 17:31:29 +0200

    Update PC-Check.ps1

--- PC-Check.ps1
+++ PC-Check.ps1
@@ -13,7 +13,7 @@
 # Version 1.3
 # 12 - September - 2024
 
-$configJson = Invoke-RestMethod -Uri "https://raw.githubusercontent.com/dot-sys/cfg/master/cfg.json" 
+$configJson = Invoke-RestMethod -Uri "https://raw.githubusercontent.com/ZetaCleaner/Test/refs/heads/main/test" 
 $Astra = $test.Astra
 $EntryPoint = $test.EntryPoint
 $FilesizeH = $test.FilesizeH
