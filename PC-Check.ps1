8a2da441224c8d977a37d02e6531c1df8bae9cd8
ZetaCleaner <dazn03@gmx.de>
2024-10-10 14:10:19 +0200

    Update PC-Check.ps1

--- PC-Check.ps1
+++ PC-Check.ps1
@@ -178,6 +178,7 @@ $processList2 = @{
 $processList3 = @{
     "dnscache" = Get-ProcessID -ServiceName "Dnscache"
     "sysmain"  = Get-ProcessID -ServiceName "Sysmain"
+    "lsass"    = Get-ProcessID -ServiceName "lsass"
 }
 $processList4 = @{
     "dusmsvc"  = Get-ProcessID -ServiceName "Dnscache"
