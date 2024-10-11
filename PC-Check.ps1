610beae25bd309af80e6914e84e5259ff8c022e2
ZetaCleaner <dazn03@gmx.de>
2024-10-11 11:13:58 +0200

    Update PC-Check.ps1

--- PC-Check.ps1
+++ PC-Check.ps1
@@ -56,7 +56,7 @@ $h5 = & { $l1; "|   Executables   |"; $l2; }
 Clear-Host
 if ((Read-Host "`n`n`nThis program requires 1GB of free disk space on your System Disk.`n`n`nWe will be downloading the programs: `n`n- ESEDatabaseView by Nirsoft `n- strings2 by Geoff McDonald (more infos at split-code.com) `n- ACC Parser, PECmd, EvtxCmd, SBECmd, SQLECmd, RECmd and WxTCmd from Eric Zimmermans Tools (more infos at ericzimmerman.github.io).`n`nThis will be fully local, no data will be collected.`nIf Traces of Cheats are found, you are highly advised to reset your PC or you could face repercussions on other Servers.`nRunning PC Checking Programs, including this script, outside of PC Checks may have impact on the outcome.`nDo you agree to a PC Check and do you agree to download said tools? (Y/N)") -eq "Y") {
     Clear-Host
-    Write-Host "`n`n`n-------------------------"-ForegroundColor red
+    Write-Host "`n`n`n-------------------------"-ForegroundColor blue
     Write-Host "|    Download Assets    |" -ForegroundColor red
     Write-Host "|      Please Wait      |" -ForegroundColor red
     Write-Host "-------------------------`n"-ForegroundColor red
@@ -171,14 +171,14 @@ $processList1 = @{
     "WSearch"   = Get-ProcessID -ServiceName "WSearch"
 }
 $processList2 = @{
-    "PcaSvc"   = (Get-Process PcaSvc).Id
+    "PcaSvc"   = Get-ProcessID -ServiceName "PcaSvc"
     "explorer" = (Get-Process explorer).Id
     "dwm"      = (Get-Process dwm).Id
 }
 $processList3 = @{
     "dnscache" = Get-ProcessID -ServiceName "Dnscache"
     "sysmain"  = Get-ProcessID -ServiceName "Sysmain"
-    "lsass"    = Get-ProcessID -ServiceName "lsass"
+    "lsass"    = (Get-Process lsass).Id
 }
 $processList4 = @{
     "dusmsvc"  = Get-ProcessID -ServiceName "Dnscache"
